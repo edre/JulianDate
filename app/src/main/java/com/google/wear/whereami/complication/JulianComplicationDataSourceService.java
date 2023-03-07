66cd3157e09f591a87aea98c8b042e7a944b0a3f
Eric Roshan-Eisner <eric.d.eisner@gmail.com>
2023-03-06 16:57:21 -0800

    Convert to java

---
+++ app/src/main/java/com/google/wear/whereami/complication/JulianComplicationDataSourceService.java
@@ -0,0 +1,56 @@
+package com.google.wear.whereami.complication;
+
+import android.os.RemoteException;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.wear.watchface.complications.data.ComplicationData;
+import androidx.wear.watchface.complications.data.ComplicationType;
+import androidx.wear.watchface.complications.data.LongTextComplicationData;
+import androidx.wear.watchface.complications.data.PlainComplicationText;
+import androidx.wear.watchface.complications.data.ShortTextComplicationData;
+import androidx.wear.watchface.complications.datasource.ComplicationDataSourceService;
+import androidx.wear.watchface.complications.datasource.ComplicationRequest;
+
+import java.time.Instant;
+
+public class JulianComplicationDataSourceService extends ComplicationDataSourceService {
+    public JulianComplicationDataSourceService() {
+        super();
+        Log.w("Service constructor", "xxx");
+    }
+
+    public static String getJulianDate(@NonNull Instant instant) {
+        long beats = instant.toEpochMilli() / 864 + 244058750000L;
+        return (beats / 100000) + "\n." + (beats % 100000);
+    }
+
+    @Override
+    public void onComplicationRequest(@NonNull ComplicationRequest request, @NonNull ComplicationRequestListener listener) {
+        Log.w("OnComplicationRequest", "xxx");
+        try {
+            listener.onComplicationData(getComplicationData(request.getComplicationType()));
+        } catch (RemoteException exception) {}
+    }
+
+    private static ComplicationData getComplicationData(@NonNull ComplicationType complicationType) {
+        String jd = getJulianDate(Instant.now());
+        PlainComplicationText label = new PlainComplicationText.Builder("Julian Date").build();
+        switch (complicationType) {
+            case SHORT_TEXT:
+                return new ShortTextComplicationData.Builder(
+                        new PlainComplicationText.Builder(jd.substring(8,12)).build(),label).build();
+            case LONG_TEXT:
+            default:
+                return new LongTextComplicationData.Builder(
+                        new PlainComplicationText.Builder(jd).build(),label).build();
+        }
+    }
+
+    @Nullable
+    @Override
+    public ComplicationData getPreviewData(@NonNull ComplicationType complicationType) {
+        return getComplicationData(complicationType);
+    }
+}
