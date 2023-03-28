b6f22e596b05e677e0f691995573a994cd711ce7
Eric Roshan-Eisner <eric.d.eisner@gmail.com>
2023-03-27 21:53:51 -0700

    Add parallel LocalTime ticker to JulianActivity.

--- app/src/main/java/JulianActivity.java
+++ app/src/main/java/JulianActivity.java
@@ -1,17 +1,23 @@
 package homa.rano.juliandate;
 
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_TIME;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class JulianActivity extends Activity {
     private Handler handler;
-    private Runnable runnable;
 
     // TODO: dedup implementations
     public static String getJulianDate(@NonNull Instant instant) {
@@ -21,23 +27,24 @@ public class JulianActivity extends Activity {
 
     private void redraw() {
         TextView textView = findViewById(R.id.text);
-        // TODO: add local time
-        textView.setText(getJulianDate(Instant.now()));
+        String isoTime = LocalDate.now().format(ISO_LOCAL_DATE) + "\n" +
+                LocalTime.now().format(ISO_LOCAL_TIME).substring(0,8);
+        textView.setText("\n" + isoTime + "\n\n" + getJulianDate(Instant.now()));
     }
 
-    private void scheduleNext() {
+    private void scheduleRepeating(long intervalMillis) {
         // Define the code block to be executed
-        runnable = new Runnable() {
+        Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 redraw();
-                // Repeat this the same runnable code block again another 2 seconds
-                handler.postDelayed(runnable, 864);
+                // Repeat the same runnable code block at the start of the next interval.
+                long next = (SystemClock.uptimeMillis()/intervalMillis + 1) * intervalMillis;
+                handler.postAtTime(this, next);
             }
         };
-        // Start the initial runnable task by posting through the handler
-        // TODO: calculate remaining time until update
-        handler.postDelayed(runnable, 864);
+        // Start the initial runnable task.
+        runnable.run();
     }
 
     @Override
@@ -50,13 +57,13 @@ public class JulianActivity extends Activity {
     @Override
     public void onPause() {
         super.onPause();
-        handler.removeCallbacks(runnable);
+        handler.removeCallbacks(null);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        redraw();
-        scheduleNext();
+        scheduleRepeating(864);
+        scheduleRepeating(1000);
     }
 }
